1518a2852e78f82c582bfacef9f006b48dc03ec0
Skywalker5276 <100366106+Skywalker5276@users.noreply.github.com>
2022-03-01 10:54:18 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 input.onButtonPressed(Button.A, function () {
     Pitch += 1
-    basic.showString("" + (Pitch))
+    basic.showNumber(Pitch)
 })
 input.onButtonPressed(Button.B, function () {
     Strike += 1
-    basic.showString("" + (Strike))
+    basic.showNumber(Strike)
 })
 let Strike = 0
 let Pitch = 0
@@ -12,12 +12,12 @@ basic.showIcon(IconNames.Yes)
 basic.forever(function () {
     if (Pitch == 4) {
         Pitch = 0
-        basic.showString("" + (Pitch))
+        basic.showNumber(Pitch)
     }
 })
 basic.forever(function () {
     if (Strike == 3) {
         Strike = 0
-        basic.showString("" + (Strike))
+        basic.showNumber(Strike)
     }
 })
